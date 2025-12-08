b409654966caf406e7b161351c1c97ab9fa67d2a
retr0lbb <retr0lbb@gmail.com>
2025-12-08 01:10:53 -0300

    feat: add new musics

--- src/app/api/genzxalpha/route.ts
+++ src/app/api/genzxalpha/route.ts
@@ -9,7 +9,6 @@ export async function GET(req: Request) {
     return NextResponse.json({ error: "Missing generation" }, { status: 400 });
   }
 
-  // 🔥 LISTAS DE MÚSICAS FIXAS (com title → será convertido para track)
   const GEN_Z = [
     {
       title: "FE!N",
@@ -19,37 +18,65 @@ export async function GET(req: Request) {
       cover: "https://e-cdns-images.dzcdn.net/images/cover/xxxx/250x250.jpg",
     },
     {
-      title: "Rich Baby Daddy",
-      artist: "Drake",
+      title: "BIRDS OF A FEATHER",
+      artist: "Billie Eilish",
       preview:
-        "https://cdnt-preview.dzcdn.net/api/1/1/c/7/f/0/c7f452be42181a050a57540cfd317f5e.mp3?hdnea=exp=1765158054~acl=/api/1/1/c/7/f/0/c7f452be42181a050a57540cfd317f5e.mp3*~data=user_id=0,application_id=42~hmac=c05c684c07f78574731fea86f897744ac43c7cd93e70274df01c03cb11a03c2d",
+        "https://cdnt-preview.dzcdn.net/api/1/1/5/e/0/0/5e0fb99f6f1e0f18ab70a4c0d18efa5c.mp3?hdnea=exp=1765166915~acl=/api/1/1/5/e/0/0/5e0fb99f6f1e0f18ab70a4c0d18efa5c.mp3*~data=user_id=0,application_id=42~hmac=84f5d072bbc8288c5aa298431010698e483bd46c4db590039649e0460c136e46",
+      cover: "https://e-cdns-images.dzcdn.net/images/cover/xxxx/250x250.jpg",
+    },
+    {
+      title: "How Sweet",
+      artist: "NewJeans",
+      preview:
+        "https://cdnt-preview.dzcdn.net/api/1/1/8/d/9/0/8d91a96b1acc6781fc44726301ea7ed2.mp3?hdnea=exp=1765167016~acl=/api/1/1/8/d/9/0/8d91a96b1acc6781fc44726301ea7ed2.mp3*~data=user_id=0,application_id=42~hmac=3b2053a2f59b91844d2f5050154acc5e1f132b7cedb0f8eaaa34f06bd01bb05b",
       cover: "https://e-cdns-images.dzcdn.net/images/cover/xxxx/250x250.jpg",
     },
   ];
 
   const GEN_ALPHA = [
     {
-      title: "Let’s Go 4",
+      title: "Oh Garota Eu Quero Você Só Pra Mim.",
       artist: "Oruam",
       preview:
-        "https://cdnt-preview.dzcdn.net/api/1/1/c/5/6/0/c56c19c8c5692e670eece0d2b9def697.mp3?hdnea=exp=1765158106~acl=/api/1/1/c/5/6/0/c56c19c8c5692e670eece0d2b9def697.mp3*~data=user_id=0,application_id=42~hmac=4c3908e06e6d718e2e80631d355a6f89e217bbf5b73c8f10d3d9d09956331cab",
+        "https://cdnt-preview.dzcdn.net/api/1/1/1/e/f/0/1ef1c8ec65110ce515c559fcc7bafe15.mp3?hdnea=exp=1765167460~acl=/api/1/1/1/e/f/0/1ef1c8ec65110ce515c559fcc7bafe15.mp3*~data=user_id=0,application_id=42~hmac=a59fe5c79d4d5d125700c2a47f77a78de6021550a4822b9f08a3c84511139f9c",
       cover: "https://e-cdns-images.dzcdn.net/images/cover/xxxx/250x250.jpg",
     },
     {
-      title: "Coração Gelado 2",
-      artist: "DJ Boy",
+      title: "PASSO BEM SOLTO",
+      artist: "ATLXS",
       preview:
-        "https://cdnt-preview.dzcdn.net/api/1/1/1/0/5/0/1053c50041fe543ef8512c7a75ee8a75.mp3?hdnea=exp=1765158143~acl=/api/1/1/1/0/5/0/1053c50041fe543ef8512c7a75ee8a75.mp3*~data=user_id=0,application_id=42~hmac=170b6331ee10021e474110e27b80ba7b7953e07aa823664874b39e470decc980",
+        "https://cdnt-preview.dzcdn.net/api/1/1/5/5/9/0/55900afd59d5c610926837fd8b031040.mp3?hdnea=exp=1765167247~acl=/api/1/1/5/5/9/0/55900afd59d5c610926837fd8b031040.mp3*~data=user_id=0,application_id=42~hmac=0124436b751b3cee9e6284fc982c0b294a825aba3906d860bd5575db8b99f436",
+      cover: "https://e-cdns-images.dzcdn.net/images/cover/xxxx/250x250.jpg",
+    },
+    {
+      title: "Young Black & Rich",
+      artist: "Melly Mike",
+      preview:
+        "https://cdnt-preview.dzcdn.net/api/1/1/6/8/2/0/6822b9c2a39b1a1a19e683f4e6238469.mp3?hdnea=exp=1765167548~acl=/api/1/1/6/8/2/0/6822b9c2a39b1a1a19e683f4e6238469.mp3*~data=user_id=0,application_id=42~hmac=5de40b782b9a528aa35a67d1be4b3016a3ca2d91d9c46179f520e5f6c306bc42",
       cover: "https://e-cdns-images.dzcdn.net/images/cover/xxxx/250x250.jpg",
     },
   ];
 
   const GEN_X = [
     {
-      title: "Caneta Azul VII",
-      artist: "Manoel Gomes",
+      title: "Smells Like Teen Spirit",
+      artist: "Nirvana",
       preview:
-        "https://cdnt-preview.dzcdn.net/api/1/1/0/c/f/0/0cf6f8c11a69c0e30e8bcf9655b3d621.mp3?hdnea=exp=1765158287~acl=/api/1/1/0/c/f/0/0cf6f8c11a69c0e30e8bcf9655b3d621.mp3*~data=user_id=0,application_id=42~hmac=29383585e6b8c4759d15c5cac9b73788300ef4d46fe6a580ebc1f9a65625a809",
+        "https://cdnt-preview.dzcdn.net/api/1/1/3/4/b/0/34bad352aa1aa0f8eecbbc9c27a40eaf.mp3?hdnea=exp=1765167699~acl=/api/1/1/3/4/b/0/34bad352aa1aa0f8eecbbc9c27a40eaf.mp3*~data=user_id=0,application_id=42~hmac=97c9d9658aa8472a7d48a71ad56a846479ea546e2690dab5554dbfaa08054c4d",
+      cover: "https://e-cdns-images.dzcdn.net/images/cover/xxxx/250x250.jpg",
+    },
+    {
+      title: "Song 2",
+      artist: "Blur",
+      preview:
+        "https://cdnt-preview.dzcdn.net/api/1/1/b/5/2/0/b52000f4b656776924aeecb6b45eef79.mp3?hdnea=exp=1765167788~acl=/api/1/1/b/5/2/0/b52000f4b656776924aeecb6b45eef79.mp3*~data=user_id=0,application_id=42~hmac=04adc363a9e1f6577dc68ce1a70cd7cb2d4eee7341988735731ae3cfbc1b1fa6",
+      cover: "https://e-cdns-images.dzcdn.net/images/cover/xxxx/250x250.jpg",
+    },
+    {
+      title: "Proibida Pra Mim",
+      artist: "Charlie Brown Jr.",
+      preview:
+        "https://cdnt-preview.dzcdn.net/api/1/1/7/5/1/0/751a5c5cb9661aed45c5490c8f10a051.mp3?hdnea=exp=1765167846~acl=/api/1/1/7/5/1/0/751a5c5cb9661aed45c5490c8f10a051.mp3*~data=user_id=0,application_id=42~hmac=7272bdc759aff07db480f1c5b95806f2f7a17b15bd9ee095a7611afa4d2ed7bc",
       cover: "https://e-cdns-images.dzcdn.net/images/cover/xxxx/250x250.jpg",
     },
   ];
